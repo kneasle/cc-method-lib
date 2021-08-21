d938e3451c52bcd3fd00d8e04e985cb10c3aabec
Kneasle <kneasle@gmail.com>
2021-08-22 00:57:01 +0100

    Add proper arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,38 @@
 //! Utility to generate a condensed library file (that can be queried quickly) from the CCCBR's XML
 //! format (which contains lots of irrelevant information and is hard to query).
 
-use std::{env::args, time::Instant};
+use argh::FromArgs;
+use std::{path::PathBuf, time::Instant};
 
 use bellframe::parse_cc_lib;
 
+/// Utility to convert the CCCBR's verbose method library into the compact format read by `bellframe`
+#[derive(FromArgs, Debug, Clone)]
+struct CliArgs {
+    /// the location of the Central Council's XML method library
+    #[argh(positional)]
+    cc_lib_path: PathBuf,
+
+    /// the location of the output file.  Defaults to "cccbr-methods.json"
+    #[argh(option, short = 'o', default = "\"cccbr-methods.json\".into()")]
+    output_path: PathBuf,
+}
+
 fn main() {
+    // Parse the CLI args
+    let args: CliArgs = argh::from_env();
+
     // Parse the input XML file
-    println!("Parsing XML...");
+    println!("Reading & parsing XML library...");
     let start_time = Instant::now();
-    let input_path = args()
-        .nth(1)
-        .expect("Please specify a file-name for the CCCBR method library's XML file.");
-    let xml = std::fs::read_to_string(input_path).expect("Error reading XML file");
+    let xml = std::fs::read_to_string(&args.cc_lib_path).expect("Error reading XML file");
     let lib = parse_cc_lib(&xml);
-    println!("done in {:?}", Instant::now() - start_time);
+    println!("done in {:?}", start_time.elapsed());
 
     // Deserialize the library, and write it to disk
     println!("\nSerialising to JSON...");
     let start_time = Instant::now();
     let json = lib.to_json().unwrap();
-    std::fs::write("cccbr-methods.json", &json).expect("Error writing JSON file");
-    println!("done in {:?}", Instant::now() - start_time);
+    std::fs::write(args.output_path, &json).expect("Error writing JSON file");
+    println!("done in {:?}", start_time.elapsed());
 }
